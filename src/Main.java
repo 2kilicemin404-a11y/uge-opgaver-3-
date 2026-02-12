856b5637cbb4356f669ebc89c1fd96715cc8c531
eminkilic <kilicemin404@gmail.com>
2026-02-12 14:30:21 +0100

    Opgave 2
    i denne opgave har jeg gjort følgende:
    
    oprettet et program, der holder styr på points fra forskellige afleveringer og beregner den samlede score

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,66 @@
-// Opgave 1 bankkonto "Emin"
+//// Opgave 1 bankkonto "Emin"
+//
+//double balance = 0;
+//
+//String accountName = "Emin";
+//
+//void main(){
+//    accountName = "Emin";
+//deposit100();
+//deposit100();
+//withdraw50();
+//printBalance();
+//
+//}
+//
+//void printBalance(){
+//System.out.println("saldo: " + accountName);
+//   System.out.println("kr: " + balance);;
+//}
+//
+//void deposit100(){
+//balance = balance +100;
+//}
+//
+//void withdraw50(){
+//balance = balance -50;
+//}
 
-double balance = 0;
 
-String accountName = "Emin";
+
+
+
+        // opgave 2 Karakter - beregner
+int assignmentPoints = 0;
+
+int examPoints = 0;
+
+int projectPoints = 0;
+
+
+
 
 void main(){
-    accountName = "Emin";
-deposit100();
-deposit100();
-withdraw50();
-printBalance();
-
+addAssignmentsPoints(25);
+addExamPoints(40);
+addProjectPoints(30);
+printTotal();
 }
 
-void printBalance(){
-System.out.println("saldo: " + accountName);
-   System.out.println("kr: " + balance);;
+void addAssignmentsPoints(int points) {
+ assignmentPoints = assignmentPoints + points;
 }
 
-void deposit100(){
-balance = balance +100;
+void addExamPoints(int points){
+ examPoints = examPoints + points;
 }
 
-void withdraw50(){
-balance = balance -50;
+void addProjectPoints(int points){
+    projectPoints = projectPoints + points;
 }
 
+void printTotal(){
+ int total = projectPoints + examPoints + assignmentPoints;
+ System.out.println("score: " + total);
+
+}
